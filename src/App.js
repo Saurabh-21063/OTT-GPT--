26f67cd2e940a3b771b2c1d1044b0d04aa9906f1
Saurabh-21063 <123747524+Saurabh-21063@users.noreply.github.com>
2024-08-03 07:48:49 +0530

    sign up form and login header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import "./App.css";
-
+import Body from "./components/Body";
 function App() {
-  return <div className="font-3xl font-bold text-green-800">ott gpt--</div>;
+  return <Body />;
 }
 
 export default App;
